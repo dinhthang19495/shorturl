1225d026b24e4d6a5a91867bd6b097d80a598cfe
Thang Dinh Nguyen <thangnguyen@Thangs-MacBook-Pro.local>
2018-12-27 12:11:53 -0800

    added psuedo code for short.py

---
+++ short.py
@@ -0,0 +1,24 @@
+# Class replaceUrl
+
+"""
+    We will take a long url and return a short url.
+    Then, users can use the short url to take them to the long url.
+"""
+
+# function to convert long url to short url
+
+# function to convert a short url to a large url
+
+# random function generator short url
+
+# function to check if a short url has been created
+
+# reduceurl.com/aaaaa
+
+# 5 characters/digits to make 916,132,832 url
+
+# we will use a-z, 0-9, and A-Z
+
+# we expect to generate 1 million url in 6 months.
+
+#
