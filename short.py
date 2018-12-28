2bdd2266986795458acbee0615773bafdccba75d
Thang Dinh Nguyen <thangnguyen@Thangs-MacBook-Pro.local>
2018-12-27 22:47:45 -0800

    create user interface and make short.py look neat

--- short.py
+++ short.py
@@ -4,85 +4,113 @@ class ReplaceUrl:
         We will take a long url and return a short url.
         Then, users can use the short url to take them to the long url.
     """
-    base_reduce_url = {0:'a', 1:'b', 2:'c', 3:'d', 4:'e', 5:'f',6:'g',7:'h',8:'i',9:'j',10:'k',
-                       11:'l',12:'m',13:'n',14:'o',15:'p',16:'q',17:'r',18:'s',19:'t',20:'u',
-                       21:'v',22:'w',23:'x',24:'y',25:'z',26:'A',27:'B',28:'C',29:'D',30:'E',
-                       31:'F',32:'G',33:'H',34:'I',35:'J',36:'K',37:'L',38:'M',39:'N',40:'O',
-                       41:'P',42:'Q',43:'R',44:'S',45:'T',46:'U',47:'V',48:'W',49:'X',50:'Y',
-                       51:'Z',52:'0',53:'1',54:'2',55:'3',56:'4',57:'5',58:'6',59:'7',60:'8',
-                       61:'9'}
+    base_reduce_url = {0: 'a', 1: 'b', 2: 'c', 3: 'd', 4: 'e', 5: 'f', 6: 'g', 7: 'h', 8: 'i', 9: 'j', 10: 'k',
+                       11: 'l', 12: 'm', 13: 'n', 14: 'o', 15: 'p', 16: 'q', 17: 'r', 18: 's', 19: 't', 20: 'u',
+                       21: 'v', 22: 'w', 23: 'x', 24: 'y', 25: 'z', 26: 'A', 27: 'B', 28: 'C', 29: 'D', 30: 'E',
+                       31: 'F', 32: 'G', 33: 'H', 34: 'I', 35: 'J', 36: 'K', 37: 'L', 38: 'M', 39: 'N', 40: 'O',
+                       41: 'P', 42: 'Q', 43: 'R', 44: 'S', 45: 'T', 46: 'U', 47: 'V', 48: 'W', 49: 'X', 50: 'Y',
+                       51: 'Z', 52: '0', 53: '1', 54: '2', 55: '3', 56: '4', 57: '5', 58: '6', 59: '7', 60: '8',
+                       61: '9'}
     BASE = 62
-    #our url
+    # our url
     URL_TEMPLATE = "http://www.uRshort.us/"
+
     def __init__(self):
         self.count = 0
         self.urls = {}
 
     # function to convert long url to short url
-    def convert_long_to_short(self, longUrl: str):
+    def convert_long_to_short(self, long_url: str):
         # Check if the large url is in the dict
-        if longUrl in self.urls:
+        if long_url in self.urls:
             # if it already exists, do sth
-            return self.URL_TEMPLATE + self.convert_count_to_reducedurl(self.urls.get(longUrl))
+            return self.URL_TEMPLATE + self.convert_count_to_reduced_url(self.urls.get(long_url))
 
-        self.urls[longUrl] = self.count
+        self.urls[long_url] = self.count
 
-        # get the count and convert it to the reduceurl
-        reducedurl = self.convert_count_to_reducedurl(self.count)
+        # get the count and convert it to the reduced_url
+        reduced_url = self.convert_count_to_reduced_url(self.count)
 
         # increment
         self.count = self.count + 1
-
         # return the url
-        return self.URL_TEMPLATE + reducedurl
+        return self.URL_TEMPLATE + reduced_url
     # function to convert a short url to a large url
-    def convert_short_to_long(self, reducedurl: str):
-        if reducedurl.startswith(ReplaceUrl.URL_TEMPLATE):
-            cnt = self.convert_reducedurl_to_number(reducedurl)
-            for k,v in self.urls.items():
+
+    def convert_short_to_long(self, reduced_url: str):
+        # when the short url start with http://www.uRshort.us/, do this
+        if reduced_url.startswith(ReplaceUrl.URL_TEMPLATE):
+            # convert the short url to number
+            cnt = self.convert_reduced_url_to_number(reduced_url)
+            # if the number matches the value
+            for k, v in self.urls.items():
                 if v == cnt:
                     return k
+        # if the number doesn't match the value
         return "It is not in the database"
 
-    # function to convert count to reduceurl
-    def convert_count_to_reducedurl(self, count: int):
+    # function to convert count to reduced url
+    def convert_count_to_reduced_url(self, count: int):
         values = []
+        # if count = 0, match it with the position 0 from base reduce url
         if count == 0:
             return ReplaceUrl.base_reduce_url.get(count)
+        # if count > 0
         while count > 0:
+            # insert the reminder value to the first position of values
             values.insert(0, count % ReplaceUrl.BASE)
+            # get the whole number of count
             count = count // ReplaceUrl.BASE
         return self.read_the_list(values)
     # function to read through the list
-    def read_the_list(self, values: list):
-        shorturl = ""
-        for i in values:
-            shorturl +=(ReplaceUrl.base_reduce_url.get(i))
-        return shorturl
 
-    # function to convert reduceurl to number
-    def convert_reducedurl_to_number(self, reducedurl: str):
-        reducedurl = ReplaceUrl.get_short_url(reducedurl)
+    @staticmethod
+    def read_the_list(values: list):
+        short_url = ""
+        # for loop
+        for i in values:
+            # get the value from values and match it with the value from base reduce url, then add it to string short url
+            short_url += (ReplaceUrl.base_reduce_url.get(i))
+        return short_url
+
+    # function to convert reduced url to number
+    @staticmethod
+    def convert_reduced_url_to_number(reduced_url: str) -> int:
+        # get the full short url and only take the shorten url part
+        reduced_url_1 = ReplaceUrl.get_short_url(reduced_url)
+        # the sum
         count = 0
-        temp_len = len(reducedurl) - 1
+        # start from the position next to the last
+        temp_len = len(reduced_url_1) - 1
+        # create a new dictionary but flip key with value
         temp_dict = ReplaceUrl.flip_dict(ReplaceUrl.base_reduce_url)
-        for i in reducedurl:
+        # for loop
+        for i in reduced_url_1:
+            # set the value to i position in temp dictionary
             value = temp_dict.get(i)
+            # add the value to the sum
             count += value * (ReplaceUrl.BASE**temp_len)
+            # decrement the temp length
             temp_len -= 1
         return count
 
     # function to flip the dictionary
-    def flip_dict(map: dict):
+    @staticmethod
+    def flip_dict(new_list: dict):
+        # create a new dictionary
         new_dict = {}
-        for k,v in map.items():
+        # From key and value from the dictionary, we create a new dictionary but flip key with value.
+        for k, v in new_list.items():
             new_dict[v] = k
         return new_dict
     # function to get the url and return the short url
-    def get_short_url(url: str):
-        shorturl = url[len(ReplaceUrl.URL_TEMPLATE):]
-        return shorturl
-# reduceurl.com/aaaaa
+
+    @staticmethod
+    def get_short_url(new_url: str):
+        # we shorten the url by taking the part after http://www.uRshort.us/
+        short_url = new_url[len(ReplaceUrl.URL_TEMPLATE):]
+        return short_url
+# http://www.uRshort.us/aaaaa
 
 # 5 characters/digits to make 916,132,832 url
 
@@ -90,13 +118,15 @@ class ReplaceUrl:
 
 # we expect to generate 1 million url in 6 months.
 
-yo = ReplaceUrl()
 
-print(yo.convert_long_to_short('fakfoakfaofka'))
-print(yo.convert_long_to_short('face'))
-print(yo.convert_long_to_short('face'))
-print(yo.convert_long_to_short('face'))
-print(yo.convert_long_to_short('face'))
-print(yo.convert_short_to_long('http://www.uRshort.us/a'))
-print(yo.convert_short_to_long('http://www.uRshort.us/u'))
-print(yo.convert_short_to_long('u'))
+if __name__ == '__main__':
+    yo = ReplaceUrl()
+    print(yo.convert_long_to_short('abc'))
+    print(yo.convert_long_to_short('face'))
+    print(yo.convert_long_to_short('face'))
+    print(yo.convert_long_to_short('face'))
+    print(yo.convert_long_to_short('face'))
+    print(yo.convert_short_to_long('http://www.uRshort.us/a'))
+    print(yo.convert_short_to_long('http://www.uRshort.us/u'))
+    print(yo.convert_short_to_long('u'))
+
---
+++ ui.py
@@ -0,0 +1,16 @@
+import short
+
+
+def user_interface():
+    url = input("Please enter your url: ")
+    print(url)
+    user = short.ReplaceUrl()
+    print(user.convert_long_to_short(url))
+    print(user.convert_short_to_long("tt"))
+    print(user.convert_short_to_long("http://uRshort.us/k"))
+    print(user.convert_short_to_long('http://www.uRshort.us/a'))
+    print(user.convert_short_to_long("a"))
+
+
+if __name__ == '__main__':
+    user_interface()
